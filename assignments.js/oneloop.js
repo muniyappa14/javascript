cb8935141975a216651e2cc99972e3cad1de1822
muniyappa14 <115540610+muniyappa14@users.noreply.github.com>
2022-10-27 19:52:04 +0530

    hijiuhi

---
+++ assignments.js/oneloop.js
@@ -0,0 +1,27 @@
+/*for(i=1;i<=10;i=i+2)
+{
+    console.log(i)
+}*/
+
+/*var i=0
+while(i<=10){
+    console.log(i)
+    i=i+1
+}*/ //while loop//
+
+/*for(i=5;i<=15;i++)
+{
+    console.log(i)
+}*///for loop//
+
+/*var i=1
+while(i<=20)
+{
+console.log(i)
+i++
+}while loop*/
+
+
+
+
+
